d3a2ab965087c4ded73b35ec1449139309f17f76
isaacrome21-sys <isaac_rome21@yahoo.com>
2026-08-20 22:27:27 -0500

    Export PGA live pricing and input adapters

--- sportsedge/pga/__init__.py
+++ sportsedge/pga/__init__.py
@@ -1,21 +1,53 @@
 """PGA modeling primitives for SportsEdge."""
 
+from .live_card import CandidateMarket, LiveCardDecision, evaluate_live_card
+from .live_inputs import (
+    LiveGolferInput,
+    LiveTournamentSnapshot,
+    SourceStamp,
+    build_golfer_inputs,
+)
 from .live_model import (
     LiveWeights,
     PlayerLiveState,
     TournamentSimulationResult,
     TruthGateResult,
+    evaluate_live_truth_gate,
     live_expected_sg_per_round,
     simulate_remaining_tournament,
-    evaluate_live_truth_gate,
+)
+from .market_pricing import (
+    MarketPrice,
+    american_to_implied,
+    expected_value_per_unit,
+    full_field_no_vig,
+    full_kelly_fraction,
+    price_selection,
+    probability_to_american,
+    two_way_no_vig,
 )
 
 __all__ = [
+    "CandidateMarket",
+    "LiveCardDecision",
+    "LiveGolferInput",
+    "LiveTournamentSnapshot",
+    "SourceStamp",
     "LiveWeights",
+    "MarketPrice",
     "PlayerLiveState",
     "TournamentSimulationResult",
     "TruthGateResult",
-    "live_expected_sg_per_round",
-    "simulate_remaining_tournament",
+    "american_to_implied",
+    "build_golfer_inputs",
+    "evaluate_live_card",
     "evaluate_live_truth_gate",
+    "expected_value_per_unit",
+    "full_field_no_vig",
+    "full_kelly_fraction",
+    "live_expected_sg_per_round",
+    "price_selection",
+    "probability_to_american",
+    "simulate_remaining_tournament",
+    "two_way_no_vig",
 ]
