3e08828d8791a811a58fec21d008cac8faf4cce2
isaacrome21-sys <isaac_rome21@yahoo.com>
2026-08-24 23:03:32 -0500

    feat: export situational shared-path readouts

--- sportsedge/core/simulate/__init__.py
+++ sportsedge/core/simulate/__init__.py
@@ -2,6 +2,12 @@
 
 from .football import JointScoreSimulator, KeyNumberMarginModel
 from .football_path import EngineAPathSimulator, FootballGamePath, ScoringEvent
+from .situational import (
+    derive_both_teams_to_n,
+    derive_largest_lead,
+    derive_race_to_n,
+    derive_winning_margin_band,
+)
 
 __all__ = [
     "JointScoreSimulator",
@@ -9,4 +15,8 @@ __all__ = [
     "EngineAPathSimulator",
     "FootballGamePath",
     "ScoringEvent",
+    "derive_race_to_n",
+    "derive_largest_lead",
+    "derive_winning_margin_band",
+    "derive_both_teams_to_n",
 ]
