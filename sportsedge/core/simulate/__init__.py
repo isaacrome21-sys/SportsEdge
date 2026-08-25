8343cc8616b058cc371dfcd693d97deb84e65f0a
isaacrome21-sys <isaac_rome21@yahoo.com>
2026-08-24 22:56:42 -0500

    feat: export Engine A path primitives

--- sportsedge/core/simulate/__init__.py
+++ sportsedge/core/simulate/__init__.py
@@ -1,5 +1,12 @@
 """Shared simulation engines."""
 
 from .football import JointScoreSimulator, KeyNumberMarginModel
+from .football_path import EngineAPathSimulator, FootballGamePath, ScoringEvent
 
-__all__ = ["JointScoreSimulator", "KeyNumberMarginModel"]
+__all__ = [
+    "JointScoreSimulator",
+    "KeyNumberMarginModel",
+    "EngineAPathSimulator",
+    "FootballGamePath",
+    "ScoringEvent",
+]
