a1150a2e0108c9d016f0258ab0717ff13c34bd57
isaacrome21-sys <isaac_rome21@yahoo.com>
2026-08-25 05:35:20 -0500

    Reconcile simulation exports with main

--- sportsedge/core/simulate/__init__.py
+++ sportsedge/core/simulate/__init__.py
@@ -3,42 +3,16 @@
 from .drive_play import EngineADrivePlaySimulator, FootballPlayPath, PlayEvent, TeamDriveProfile
 from .football import JointScoreSimulator, KeyNumberMarginModel
 from .football_path import EngineAPathSimulator, FootballGamePath, ScoringEvent
-from .player_markets import derive_player_market_readouts
-from .situational import (
-    derive_both_teams_to_n,
-    derive_largest_lead,
-    derive_race_to_n,
-    derive_winning_margin_band,
-)
+from .player_markets import derive_player_market_readouts, derive_player_stat_market
+from .situational import derive_both_teams_to_n, derive_largest_lead, derive_race_to_n, derive_winning_margin_band
 from .special_teams import TeamSpecialTeamsProfile, derive_special_teams_market_readouts
-from .usage import (
-    AttributedFootballPath,
-    AttributedPlay,
-    EngineBUsageAllocator,
-    PlayerUsageProfile,
-    TeamUsageProfile,
-)
+from .usage import AttributedFootballPath, AttributedPlay, EngineBUsageAllocator, PlayerUsageProfile, TeamUsageProfile
 
 __all__ = [
-    "JointScoreSimulator",
-    "KeyNumberMarginModel",
-    "EngineAPathSimulator",
-    "FootballGamePath",
-    "ScoringEvent",
-    "EngineADrivePlaySimulator",
-    "FootballPlayPath",
-    "PlayEvent",
-    "TeamDriveProfile",
-    "PlayerUsageProfile",
-    "TeamUsageProfile",
-    "AttributedPlay",
-    "AttributedFootballPath",
-    "EngineBUsageAllocator",
-    "derive_player_market_readouts",
-    "TeamSpecialTeamsProfile",
-    "derive_special_teams_market_readouts",
-    "derive_race_to_n",
-    "derive_largest_lead",
-    "derive_winning_margin_band",
-    "derive_both_teams_to_n",
+    "JointScoreSimulator", "KeyNumberMarginModel", "EngineAPathSimulator", "FootballGamePath", "ScoringEvent",
+    "EngineADrivePlaySimulator", "FootballPlayPath", "PlayEvent", "TeamDriveProfile", "PlayerUsageProfile",
+    "TeamUsageProfile", "AttributedPlay", "AttributedFootballPath", "EngineBUsageAllocator",
+    "derive_player_stat_market", "derive_player_market_readouts", "TeamSpecialTeamsProfile",
+    "derive_special_teams_market_readouts", "derive_race_to_n", "derive_largest_lead",
+    "derive_winning_margin_band", "derive_both_teams_to_n",
 ]
