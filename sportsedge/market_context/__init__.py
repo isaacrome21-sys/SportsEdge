58d57c376b5f7c64ccfb8ae5a2ba6bbf8c361655
isaacrome21-sys <isaac_rome21@yahoo.com>
2026-09-08 09:45:40 -0500

    Export MySpariEdge NFL prop context contract

--- sportsedge/market_context/__init__.py
+++ sportsedge/market_context/__init__.py
@@ -1,5 +1,19 @@
 """Market-context sidecars that must never enter predictive model features."""
 
+from .myspariedge_props import (
+    CONTEXT_LANE as MYSPARIEDGE_CONTEXT_LANE,
+    MYSPARIEDGE_PROP_CONTRACT,
+    ModelTrendDiagnostic,
+    MySpariEdgeContextError,
+    MySpariEdgePropObservation,
+    MySpariEdgeSnapshot,
+    PropTrendMatch,
+    TrendWindow,
+    compare_to_model as compare_myspariedge_to_model,
+    find_prop_context as find_myspariedge_prop_context,
+    parse_myspariedge_records,
+    research_sidecar as myspariedge_research_sidecar,
+)
 from .public_splits import (
     PublicSplitObservation,
     PublicSplitSnapshot,
@@ -11,6 +25,18 @@ from .public_splits import (
 )
 
 __all__ = [
+    "MYSPARIEDGE_CONTEXT_LANE",
+    "MYSPARIEDGE_PROP_CONTRACT",
+    "ModelTrendDiagnostic",
+    "MySpariEdgeContextError",
+    "MySpariEdgePropObservation",
+    "MySpariEdgeSnapshot",
+    "PropTrendMatch",
+    "TrendWindow",
+    "compare_myspariedge_to_model",
+    "find_myspariedge_prop_context",
+    "myspariedge_research_sidecar",
+    "parse_myspariedge_records",
     "PublicSplitObservation",
     "PublicSplitSnapshot",
     "PublicSplitsError",
