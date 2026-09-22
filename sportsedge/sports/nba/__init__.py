3d485a2af2b620d07d5c2e0b70efbd6e6d5d75ee
isaacrome21-sys <isaac_rome21@yahoo.com>
2026-09-22 09:44:17 -0500

    NBA: clean-port bootstrap package to current main

---
+++ sportsedge/sports/nba/__init__.py
@@ -0,0 +1,5 @@
+"""NBA modeling and RUN IT surfaces.
+
+NBA is intentionally isolated from football/baseball mechanics.  Modules in this
+package may reuse shared SportsEdge pricing/provenance infrastructure only.
+"""
